3527ac537e4cb0452e08bf8c8caab111e30960a6
GabrielFacioni <gabriel.facioni@enterscience.com.br>
2022-10-02 09:55:21 -0300

    teste

--- src/components/routes/Breshop/Breshop.js
+++ src/components/routes/Breshop/Breshop.js
@@ -1,23 +1,24 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { URL } from "../../../variables";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { URL } from '../../../variables';
 
 const Breshop = () => {
   const token = useSelector((state) => state.AppReducer.token);
-  console.log("token", token);
+  console.log('token', token);
 
-  const user = localStorage.getItem("user");
-  console.log("id", user.name);
+  let user = localStorage.getItem('user');
+  user = JSON.parse(user);
+  console.log('id', user.id);
   React.useEffect(() => {
-    fetch(`${URL}api/get_breshop/${1}`, {
-      method: "GET",
+    fetch(`${URL}api/get_breshop/${user.id}`, {
+      method: 'GET',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`,
       },
       //   body: JSON.stringify({ name: "teste" }),
     }).then(async (responseLog) => {
-      console.log("resp", responseLog);
+      console.log('resp', responseLog);
     });
   }, []);
   return <div>Breshop</div>;
