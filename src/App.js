d6f9f9dfe5386cc04410a105224149d552abc612
GabrielFacioni <gabriel.facioni@enterscience.com.br>
2023-03-29 20:03:08 -0300

    fix: Login and register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import './components/pages/Styles/style.css';
-import RoutesContainer from './components/routes';
-import { ToastContent } from './components/utilities/Alerts';
+import RoutesContainer from './components/routes'
+import { createTheme, ThemeProvider } from '@mui/material'
+import { ToastContent } from './components/utilities/Alerts'
+import './components/pages/Styles/style.css'
 
 function App() {
+
+  const Theme = createTheme({
+    palette: {
+      primary: { main: '#693B9F' },
+      secondary: { main: '#1976d2' },
+      success: { main: '#693B9F' },
+    }
+  })
+
   return (
     <div className="App">
-      <RoutesContainer />
-      <ToastContent />
+      <ThemeProvider theme={Theme}>
+        <RoutesContainer />
+        <ToastContent />
+      </ThemeProvider>
     </div>
   );
 }
